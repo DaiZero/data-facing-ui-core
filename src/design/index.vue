dac721437b3ef4d864cc1cb85fd7cbacf0477988
DZero <dzd624175217@gmail.com>
2021-12-09 15:27:07 +0800

    header组件移位

--- src/design/index.vue
+++ src/design/index.vue
@@ -1,53 +1,66 @@
 <template>
-<div>
-<el-row>
-    <el-col :span="24">
-      <div class="grid-content bg-purple-dark">
-          <design-header></design-header>
-      </div>
-    </el-col>
-  </el-row>
-  <el-row>
-    <el-col :span="6">
-      <div class="grid-content bg-purple">
-        <tool-left-side></tool-left-side>
-      </div>
-    </el-col>
-    <el-col :span="12">
-      <div class="grid-content bg-purple-light">
-      <df-page></df-page>
-      </div>
-    </el-col>
-    <el-col :span="6">
-      <div class="grid-content bg-purple-light">
+<div style="width=1000px;height=800px;">
+    <el-container>
+      <el-header>
+        <tool-header></tool-header>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <tool-left-side></tool-left-side>
+        </el-aside>
+        <el-main><df-page></df-page></el-main>
         <tool-right-side></tool-right-side>
-      </div>
-    </el-col>
-  </el-row>
+      </el-container>
+    </el-container>
 </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
 import DfPage from "./pages/index.vue"
-import DesignHeader from "./components/design-header/index.vue"
+import ToolHeader from "./components/tool-header/index.vue"
 import ToolLeftSide from "./components/tool-left-side/index.vue"
 import ToolRightSide from "./components/tool-right-side/index.vue"
 
 export default defineComponent({
   name: 'Design',
-  components:{DfPage,DesignHeader,ToolLeftSide,ToolRightSide},
+  components:{DfPage,ToolHeader,ToolLeftSide,ToolRightSide},
   setup() {
   },
 })
 </script>
 <style scoped>
-.bg-purple-dark {
-  background: #99a9bf;
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  line-height: 60px;
 }
-.bg-purple {
-  background: #d3dce6;
+
+.el-aside {
+  background-color: #d3dce6;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  line-height: 200px;
 }
-.bg-purple-light {
-  background: #e5e9f2;
+
+.el-main {
+  background-color: #e9eef3;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
 }
 </style>
