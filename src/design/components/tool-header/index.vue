41e24d18b39f977ad8d7e37f6d826d630d9ff30b
DZero <dzd624175217@gmail.com>
2022-01-18 22:58:54 +0800

    update

--- src/design/components/tool-header/index.vue
+++ src/design/components/tool-header/index.vue
@@ -2,8 +2,16 @@
     <div class="design-header">
         <div class="design-header__logo">
         </div>
-        <div class="design-header__title">
-            <h1>设计头啊啊啊啊</h1>
+        <div class="design-header__title" style="display:flex">
+        <button style="background-color: aquamarine;margin-left: 10px;margin-right: 10px;">导入</button>
+        <button style="background-color: aquamarine;margin-left: 10px;margin-right: 10px;">导出</button>
+        <button style="background-color: aquamarine;margin-left: 10px;margin-right: 10px;">编辑</button>
+        <button style="background-color: aquamarine;margin-left: 10px;margin-right: 10px;">撤销</button>
+        <button style="background-color: aquamarine;margin-left: 10px;margin-right: 10px;">预览</button>
+        <button style="background-color: aquamarine;margin-left: 10px;margin-right: 10px;">保存</button>
+        <button style="background-color: aquamarine;margin-left: 10px;margin-right: 10px;">另存</button>
+        <button style="background-color: aquamarine;margin-left: 10px;margin-right: 10px;">发版</button>
+        <button style="background-color:black;margin-left: 10px;margin-right: 10px;">退出</button>
         </div>
     </div>
 </template>
